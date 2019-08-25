dd6fca9df72a67559d2eae0eeefa7aca741770be
reallyusefulengine <will.jh75@gmail.com>
2019-08-25 22:26:57 +1000

    added profile image functionality

--- blogger/users/models.py
+++ blogger/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        im = img.convert("RGB")
+
+        if im.height > 300 or im.width > 300 :
+            output_size = (300, 300)
+            im.thumbnail(output_size)
+            im.save(self.image.path)
